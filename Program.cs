2b6d1b4752a05fb3e5c6ac486553ca13f2c19293
LDanielli <jichuuss@gmail.com>
2025-11-12 20:28:49 -0300

    add files

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        string[] vetor = { "Jihyo", "Mina", "Nayeon", "Sana", "Tzuyu", "Chaeyoung", "Dahyun", "Jeongyeon", "Momo","1", "2", "3","4", "5", "6"};
+        
+        Console.Write("Digite o nome ou número que deseja buscar: ");
+        string busca = Console.ReadLine();
+
+        
+        bool encontrado = false;
+
+        
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i].Equals(busca, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"'{busca}' encontrado na posição {i} do vetor.");
+                encontrado = true;
+                break; 
+            }
+        }
+
+        // Caso não tenha encontrado
+        if (!encontrado)
+        {
+            Console.WriteLine("Valor não encontrado no vetor.");
+        }
+
+        Console.ReadKey();
+    }
+}
